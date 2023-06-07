ea9263f164b1120ea12e8763238b1bea7077c5e5
Weslley-Prado <weslley.prado73@gmail.com>
2023-06-07 13:02:02 -0300

    feat: create pagination

--- api-spring/src/main/java/com/trainning/udemy/services/PersonServices.java
+++ api-spring/src/main/java/com/trainning/udemy/services/PersonServices.java
@@ -3,10 +3,11 @@ package com.trainning.udemy.services;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-import java.util.List;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.trainning.udemy.controllers.PersonController;
@@ -29,15 +30,16 @@ public class PersonServices {
 	@Autowired
 	PersonRepository repository;
 
-	public List<PersonVO> findAll() {
+	public Page<PersonVO> findAll(Pageable pageable) {
 
 		logger.info("Finding all people!");
-
-		var persons = DozerMapper.parseListObjects(repository.findAll(), PersonVO.class);
-		persons
-			.stream()
-			.forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
-		return persons;
+		
+		var personPage = repository.findAll(pageable);
+        var personVosPage = personPage.map(p -> DozerMapper.parseObject(p, PersonVO.class));
+        personVosPage.map(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
+        
+	
+		return personVosPage;
 	}
 
 	public PersonVO findById(Long id) {
